ec8b760e841bfa9ac021b74f77ac4c59775a48f6
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-08-26 20:38:58 +0530

    Update lc.py

--- lc.py
+++ lc.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+import time 
 from bs4 import BeautifulSoup
 
 # Define the chromedriver service
