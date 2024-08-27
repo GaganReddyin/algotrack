dbe7c520d9aa945941a13e76691503b2915bd6ee
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-08-27 15:31:41 +0530

    Update lc_explore.py

--- lc_explore.py
+++ lc_explore.py
@@ -1,6 +1,6 @@
 # Import required packages
 import os        
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
