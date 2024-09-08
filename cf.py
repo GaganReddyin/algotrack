916e7debd4a4414a5d103d67e3d850126f3fabe7
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-09-08 12:28:11 +0530

    Update cf.py

--- cf.py
+++ cf.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager 
 import time
 from bs4 import BeautifulSoup
 
