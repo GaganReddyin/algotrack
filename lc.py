90704846934b4896cd50ce3dbde6009ae75483eb
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-09-08 12:29:00 +0530

    Update lc.py

--- lc.py
+++ lc.py
@@ -2,7 +2,7 @@
 from selenium import webdriver     
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time 
