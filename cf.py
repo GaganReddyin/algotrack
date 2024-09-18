e50aa86f646d5f2eb45d3d3765d2f8c5588121e7
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-09-18 15:55:51 +0530

    Update cf.py

--- cf.py
+++ cf.py
@@ -1,7 +1,7 @@
 # Import required packages
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from webdriver_manager.chrome import ChromeDriverManager 
