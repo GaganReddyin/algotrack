2324a074ac79af14d47bd0d7641aa847071f9338
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-12-27 04:45:48 +0530

    Update lc_explore.py

--- lc_explore.py
+++ lc_explore.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup   
 
 # Define the chromedriver service
 s = Service('chromedriver.exe')
